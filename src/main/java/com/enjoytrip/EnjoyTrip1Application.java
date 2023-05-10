d347e1c26ac3d55888f54d0e454fa1cedd850a79
이상재 <tkdwo7699@gmail.com>
2023-05-10 13:22:58 +0900

    init : SpringBoot Project 재생성
    SpringBoot 버전 2.7.11 Java 8

---
+++ src/main/java/com/enjoytrip/EnjoyTrip1Application.java
@@ -0,0 +1,13 @@
+package com.enjoytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyTrip1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyTrip1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/enjoytrip/EnjoyTrip1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyTrip1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
