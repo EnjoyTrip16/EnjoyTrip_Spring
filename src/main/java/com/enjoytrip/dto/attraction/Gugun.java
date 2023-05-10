d9c765a69490548bc79a7b42228879119c3a0499
이상재 <tkdwo7699@gmail.com>
2023-05-10 23:25:22 +0900

    feat : Gugun DTO 추가
    gugun table에 대한 DAO가 DB로부터 얻은 데이터를 담기 위한 DTO

---
+++ src/main/java/com/enjoytrip/dto/attraction/Gugun.java
@@ -0,0 +1,44 @@
+package com.enjoytrip.dto.attraction;
+
+public class Gugun {
+	
+	//gugun table PK
+	private Long gugunCode;
+	
+	//gugun table 구/군 이름 ex)강서구,달성구,강화군
+	private String gugunName;
+	
+	//sido table PK gugun table FK
+	private Long sidoCode;
+
+	public Long getGugunCode() {
+		return gugunCode;
+	}
+
+	public void setGugunCode(Long gugunCode) {
+		this.gugunCode = gugunCode;
+	}
+
+	public String getGugunName() {
+		return gugunName;
+	}
+
+	public void setGugunName(String gugunName) {
+		this.gugunName = gugunName;
+	}
+
+	public Long getSidoCode() {
+		return sidoCode;
+	}
+
+	public void setSidoCode(Long sidoCode) {
+		this.sidoCode = sidoCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Gugun 구/군 정보 [gugunCode=" + gugunCode + ", gugunName=" + gugunName + ", sidoCode=" + sidoCode + "]";
+	}
+	
+	
+}
