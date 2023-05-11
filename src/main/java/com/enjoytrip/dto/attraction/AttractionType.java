4b03a2d6bf3b0298d662ad454cae835d29182d9a
이상재 <tkdwo7699@gmail.com>
2023-05-11 17:25:55 +0900

    fix : AttractionType DTO getter setter 추가
    getter와 setter가누락되었던 부분 수정

--- src/main/java/com/enjoytrip/dto/attraction/AttractionType.java
+++ src/main/java/com/enjoytrip/dto/attraction/AttractionType.java
@@ -9,4 +9,25 @@ public class AttractionType {
 	//attraction_type 관광지 종류명
 	private String attractionType;
 
+	public long getAttractionTypeId() {
+		return attractionTypeId;
+	}
+
+	public void setAttractionTypeId(long attractionTypeId) {
+		this.attractionTypeId = attractionTypeId;
+	}
+
+	public String getAttractionType() {
+		return attractionType;
+	}
+
+	public void setAttractionType(String attractionType) {
+		this.attractionType = attractionType;
+	}
+
+	@Override
+	public String toString() {
+		return "AttractionType [attractionTypeId=" + attractionTypeId + ", attractionType=" + attractionType + "]";
+	}
+	
 }
