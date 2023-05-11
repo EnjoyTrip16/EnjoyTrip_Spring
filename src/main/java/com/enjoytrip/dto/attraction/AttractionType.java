98c445ad81aab29a68822a51bc3e2270b4533707
이상재 <tkdwo7699@gmail.com>
2023-05-11 16:39:16 +0900

    feat : 관광지 종류에 관한 DTO
    attraction_type 테이블에 대한 DAO가 DB에서 데이터를 담을 객체

---
+++ src/main/java/com/enjoytrip/dto/attraction/AttractionType.java
@@ -0,0 +1,12 @@
+package com.enjoytrip.dto.attraction;
+
+//관광지 종류에 관한 DTO
+public class AttractionType {
+	
+	//attraction_type table PK 
+	private long attractionTypeId;
+	
+	//attraction_type 관광지 종류명
+	private String attractionType;
+
+}
