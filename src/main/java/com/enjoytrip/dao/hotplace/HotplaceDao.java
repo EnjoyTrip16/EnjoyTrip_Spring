c86f6fd77504be8bcfcc0a533a6c8264b9674f25
KimJaea <rkddnjs58@gmail.com>
2023-05-20 18:24:23 +0900

    feat : 핫 플레이스 DAO 인터페이스

---
+++ src/main/java/com/enjoytrip/dao/hotplace/HotplaceDao.java
@@ -0,0 +1,23 @@
+package com.enjoytrip.dao.hotplace;
+
+import java.util.List;
+
+import com.enjoytrip.dto.hotplace.Hotplace;
+import com.enjoytrip.dto.hotplace.HotplaceSearchCondition;
+
+// 핫 플레이스 테이블 DAO
+public interface HotplaceDao {
+
+	// hotplace table 조회
+	public List<Hotplace> searchHotplace(HotplaceSearchCondition hotplaceSearchCondifion);
+	
+	// hotplace 삽입
+	public Long createHotplace(Hotplace hotplace);
+	
+	// hotplace 수정
+	public Long updateHotplace(Hotplace hotplace);
+	
+	// hotplace 삭제
+	public Long deleteHotplace(Long hotplaceId);
+	
+}
