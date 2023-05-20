ad5538ef0e10787909c492e996bae91df5bd5702
KimJaea <rkddnjs58@gmail.com>
2023-05-21 01:22:23 +0900

    feat : 핫 플레이스 댓글 DAO 작성

---
+++ src/main/java/com/enjoytrip/dao/hotplace/HotplaceCommentsDao.java
@@ -0,0 +1,28 @@
+package com.enjoytrip.dao.hotplace;
+
+import java.util.List;
+
+import com.enjoytrip.dto.hotplace.HotplaceComment;
+
+// 핫 플레이스 댓글 DAO
+public interface HotplaceCommentsDao {
+
+	// 핫 플레이스 댓글 삽입
+	public Long createHotplaceComment(HotplaceComment hotplaceComment);
+
+	// 핫 플레이스 댓글 수정
+	public Long updateHotplaceComment(HotplaceComment hotplaceComment);
+
+	// 핫 플레이스 댓글 조회
+	List<HotplaceComment> searchHotplaceComments(Long hotplaceId);
+
+	// 핫 플레이스의 총 댓글 수 조회
+	public Integer searchCommentByHotplace(Long hotplaceId);
+
+	// 사용자가 작성한 총 댓글 수 조회
+	public Integer searchCommentByUser(Long userId);
+
+	// 핫 플레이스 댓글 삭제
+	public Long deleteHotplaceComment(HotplaceComment hotplaceComment);
+
+}
