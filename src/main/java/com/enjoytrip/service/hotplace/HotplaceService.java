f2bb22b6ddea674a2e3bcb9b9e723228aff6d6d6
KimJaea <rkddnjs58@gmail.com>
2023-05-22 17:27:59 +0900

    refactor : 주석에 부연 설명 추가

--- src/main/java/com/enjoytrip/service/hotplace/HotplaceService.java
+++ src/main/java/com/enjoytrip/service/hotplace/HotplaceService.java
@@ -8,7 +8,7 @@ import com.enjoytrip.dto.hotplace.HotplaceSearchCondition;
 
 public interface HotplaceService {
 	
-	// 핫 플레이스 정보를 가져오는 메소드
+	// 조건에 맞는 핫 플레이스 정보를 가져오는 메소드
 	public List<Hotplace> searchHotplace(HotplaceSearchCondition hotplaceSearchCondition) throws SQLException;
 	
 	// 핫 플레이스 정보를 삽입하는 메소드
@@ -17,6 +17,6 @@ public interface HotplaceService {
 	// 핫 플레이스 정보를 수정하는 메소드
 	public Long updateHotplace(Hotplace hotplace) throws SQLException;
 	
-	// 핫 플레이스 정보를 삭제하는 메소드
+	// 조건에 맞는 핫 플레이스 정보를 삭제하는 메소드
 	public Long deleteHotplace(HotplaceSearchCondition hotplaceSearchCondition) throws SQLException;
 }
