faba54bc33ba0176339055c4819e1ad33c2006ec
이상재 <tkdwo7699@gmail.com>
2023-05-23 00:31:10 +0900

    feat : 계획 멤버 DAO

--- src/main/java/com/enjoytrip/dao/plan/PlanMemberDao.java
+++ src/main/java/com/enjoytrip/dao/plan/PlanMemberDao.java
@@ -1,5 +1,17 @@
 package com.enjoytrip.dao.plan;
 
-public interface PlanMemberDao {
+import java.util.List;
 
+import com.enjoytrip.dto.plan.PlanMember;
+import com.enjoytrip.dto.plan.PlanSearchCondition;
+
+public interface PlanMemberDao {
+	
+	public List<PlanMember> retrievePlanMember(PlanSearchCondition planSearchCondition);
+	
+	public Long createPlanMember(PlanMember planMember);
+	
+	public Long updatePlanMember(PlanMember planMember);
+	
+	public Long deletePlanMember(PlanSearchCondition planSearchCondition);
 }
