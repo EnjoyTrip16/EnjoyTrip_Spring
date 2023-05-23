b6634ef5e101b4a0a03d7c1d6aeacfbdb7d1799f
KimJaea <rkddnjs58@gmail.com>
2023-05-23 13:56:19 +0900

    feat : UserGradeService 구현체 UserGradeServiceImple 생성

---
+++ src/main/java/com/enjoytrip/service/user/UserGradeServiceImpl.java
@@ -0,0 +1,37 @@
+package com.enjoytrip.service.user;
+
+import java.sql.SQLException;
+
+import com.enjoytrip.dao.user.UserGradeDao;
+import com.enjoytrip.dto.user.UserGrade;
+
+public class UserGradeServiceImpl implements UserGradeService {
+
+	private UserGradeDao userGradeDao;
+
+	public UserGradeServiceImpl(UserGradeDao userGradeDao) {
+		super();
+		this.userGradeDao = userGradeDao;
+	}
+
+	@Override
+	public Long createUserGrade(UserGrade userGrade) throws SQLException {
+		return userGradeDao.createUserGrade(userGrade);
+	}
+
+	@Override
+	public String searchUserGrade(Long userId) throws SQLException {
+		return userGradeDao.searchUserGrade(userId);
+	}
+
+	@Override
+	public Long updateUserGrade(UserGrade userGrade) throws SQLException {
+		return userGradeDao.updateUserGrade(userGrade);
+	}
+
+	@Override
+	public Long deleteUserGrade(Long userId) throws SQLException {
+		return userGradeDao.deleteUserGrade(userId);
+	}
+
+}
