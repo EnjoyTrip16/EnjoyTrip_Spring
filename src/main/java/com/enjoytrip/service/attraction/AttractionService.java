9432ed95850d205eaada4897ddb863653335f45e
이상재 <tkdwo7699@gmail.com>
2023-05-24 11:37:05 +0900

    feat : 관광지 생성 서비스

--- src/main/java/com/enjoytrip/service/attraction/AttractionService.java
+++ src/main/java/com/enjoytrip/service/attraction/AttractionService.java
@@ -1,8 +1,114 @@
 package com.enjoytrip.service.attraction;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.enjoytrip.dao.attraction.AttractionDao;
+import com.enjoytrip.dao.attraction.AttractionDescriptionDao;
+import com.enjoytrip.dao.attraction.AttractionInfoDao;
+import com.enjoytrip.dao.attraction.AttractionModifyRequestDao;
+import com.enjoytrip.dto.attraction.Attraction;
+import com.enjoytrip.dto.attraction.AttractionDescription;
+import com.enjoytrip.dto.attraction.AttractionInfo;
+import com.enjoytrip.dto.attraction.AttractionSearchCondition;
+import com.enjoytrip.dto.attraction.AttractionSimpleInfo;
+import com.enjoytrip.dto.plan.response.AttractionTotalInfo;
+import com.enjoytrip.exception.AttractionException;
 
 @Service
 public class AttractionService {
 	
+	private AttractionDao attractionDao;
+	private AttractionDescriptionDao attractionDescriptionDao;
+	private AttractionInfoDao attractionInfoDao;
+	private AttractionModifyRequestDao attractionModifyRequestDao;
+	
+	
+	
+	public AttractionService(AttractionDao attractionDao, AttractionDescriptionDao attractionDescriptionDao,
+			AttractionInfoDao attractionInfoDao, AttractionModifyRequestDao attractionModifyRequestDao) {
+		super();
+		this.attractionDao = attractionDao;
+		this.attractionDescriptionDao = attractionDescriptionDao;
+		this.attractionInfoDao = attractionInfoDao;
+		this.attractionModifyRequestDao = attractionModifyRequestDao;
+	}
+
+
+
+	@Transactional
+	public Long createAttraction(AttractionTotalInfo attractionTotalInfo) throws AttractionException{
+		
+		AttractionSimpleInfo attractionSimpleInfo = attractionTotalInfo.getAttractionSimpleInfo();
+		Attraction attraction = new Attraction();
+		attraction.setattractionTypeId(
+				attractionSimpleInfo.getAttractionTypeId()
+		);
+		attraction.setTitle(
+				attractionSimpleInfo.getTitle()
+		);
+		attractionDao.createAttraction(attraction);
+		Long attractionId = attraction.getAttractionId();
+		
+		AttractionSearchCondition attractionSearchCondition = new AttractionSearchCondition();
+		attractionSearchCondition.setAttractionId(attractionId);
+		List<Attraction> attractions = attractionDao.retrieveAttraction(attractionSearchCondition);
+		
+		if(attractions.size()!=1) {
+			throw new AttractionException("관광지 생성 실패");
+		}
+		
+		AttractionDescription attractionDescription = attractionTotalInfo.getAttractionDescription();
+		attractionDescriptionDao.createAttractionDescription(attractionDescription);
+		
+		AttractionInfo attractionInfo = new AttractionInfo();
+		
+		attractionInfo.setAddr1(
+				attractionSimpleInfo.getAddr1()
+		);
+		
+		attractionInfo.setAttractionId(
+				attractionId
+		);
+		
+		attractionInfo.setFirstImage(
+				attractionSimpleInfo.getFirstImage()
+		);
+		
+		attractionInfo.setGugunCode(
+				attractionSimpleInfo.getGugunCode()
+		);
+		
+		attractionInfo.setSidoCode(
+				attractionSimpleInfo.getSidoCode()
+		);
+		
+		attractionInfo.setLatitude(
+				attractionSimpleInfo.getLatitude()
+		);
+
+		attractionInfo.setLongitude(
+				attractionSimpleInfo.getLongitude()
+		);
+
+		attractionInfo.setTel(
+				attractionSimpleInfo.getTel()
+		);
+
+		attractionInfo.setZipcode(
+				attractionSimpleInfo.getZipcode()
+		);
+		
+		attractionInfoDao.createAttractionInfo(attractionInfo);
+		
+		List<AttractionInfo> attractionInfos = attractionInfoDao.retrieveAttractionInfo(attractionSearchCondition);
+		
+		if(attractionInfos.size()!=1) {
+			throw new AttractionException("관광지 정보 생성 실패");
+		}
+		
+		return attractionId;
+	}
 }
---
+++ src/main/java/com/enjoytrip/service/attraction/AttractionTypeService.java
@@ -0,0 +1,5 @@
+package com.enjoytrip.service.attraction;
+
+public class AttractionTypeService {
+
+}
