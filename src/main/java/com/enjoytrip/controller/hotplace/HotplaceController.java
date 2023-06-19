5ecdd2e4b5b87a94f32a78d3d74fe973fe23931a
KimJaea <rkddnjs58@gmail.com>
2023-06-19 23:03:36 +0900

    feat : Hotplace Controller 생성

---
+++ src/main/java/com/enjoytrip/controller/hotplace/HotplaceController.java
@@ -0,0 +1,10 @@
+package com.enjoytrip.controller.hotplace;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotplace")
+public class HotplaceController {
+
+}
